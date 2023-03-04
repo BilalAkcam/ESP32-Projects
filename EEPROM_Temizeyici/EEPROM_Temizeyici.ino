4594ac380e510520bc0a0ba6fc6bbd8cb3dc01c4
Bilal Habeş Akçam <121240992+BilalAkcam@users.noreply.github.com>
2023-03-04 17:54:16 +0300

    Update EEPROM_Temizeyici.ino

--- EEPROM_Temizeyici/EEPROM_Temizeyici.ino
+++ EEPROM_Temizeyici/EEPROM_Temizeyici.ino
@@ -3,12 +3,10 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 for (int i = 0; i < 2048; i++) {
  EEPROM.write(i, 0);
